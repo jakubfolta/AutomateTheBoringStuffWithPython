1f46a8ebabe71d450e2a588ea7b76fe3d2da57d9
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-17 04:32:31 +0100

    first commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+#Keyword arguments and print
+print('Hello')
+print('World')
+
+print('Hello', end = '')
+print('World')
+
+print('cats', 'dogs', 'mice')
+print('cats', 'dogs', 'mice', sep = ',')
+
+#Local and global scope
+
