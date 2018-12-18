8c350986a7584c6059394d73c07c03d21b033f8f
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-18 03:38:04 +0100

    continuation

--- functions.py
+++ functions.py
@@ -9,4 +9,12 @@ print('cats', 'dogs', 'mice')
 print('cats', 'dogs', 'mice', sep = ',')
 
 #Local and global scope
+#Local variables cannot be used in the global scope
+def spam():
+    eggs = 3232
+
+spam()
+
+#print(eggs) #name error
+
 
