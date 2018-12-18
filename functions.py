6f9d730006c4aefb4d37db02acc4af8f6b64d572
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-18 03:45:10 +0100

    continuation

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 #Keyword arguments and print
+
 print('Hello')
 print('World')
 
@@ -10,11 +11,53 @@ print('cats', 'dogs', 'mice', sep = ',')
 
 #Local and global scope
 #Local variables cannot be used in the global scope
+
 def spam():
     eggs = 3232
 
 spam()
 
 #print(eggs) #name error
+
+def spam():
+    eggs = 54
+    bacon()
+    print(eggs)
+
+def bacon():
+    ham = 292
+    eggs = 0
+
+spam()
+
+#Global variables can be read from local scope
+
+def spam():
+    print(eggs)
+
+eggs = 54
+
+spam()
+
+print(eggs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
