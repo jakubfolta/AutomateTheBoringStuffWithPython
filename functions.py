d04d8d35fad13342b00c1bc56a56870dedf3ff72
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-21 04:53:42 +0100

    continuation

--- functions.py
+++ functions.py
@@ -95,6 +95,15 @@ print(spam(1))
 #The collatz sequence
 
 def collatz(number):
+    
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+
+    elif number % 2 != 0:
+        print(3 * number +1)
+        return 3 * number + 1
+        
    
 
 
