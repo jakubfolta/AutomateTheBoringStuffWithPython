7145006d44514297778587c03401c50e652c1734
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-28 20:40:37 +0100

    continuation

--- lists.py
+++ lists.py
@@ -4,7 +4,9 @@
 spam = ['dog', 'cat']
 print(spam)
 #Getting individual values in a list with indexes
-spam = ['cat', 'dog', 'rat', 'elephant']
+spam = ['cat', 'dog', 'bat', 'elephant']
 print(spam[0])
 print(spam[1])
 print(spam[2])
+
+print('The ' + spam[2] + ' ate the ' + spam[0] + '.')
