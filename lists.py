6a89987b98160a55df09ffe2d67179ef5586a7ac
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-29 15:21:45 +0100

    continuation

--- lists.py
+++ lists.py
@@ -52,6 +52,34 @@ del spam[2]
 print(spam)
 del spam[2]
 print(spam)
+#Working with lists
+catNames = []
+while True:
+    print('Enter the name of cat ' + str(len(catNames) + 1) + ' (Or enter nothing to stop): ')
+    name = input()
+    if name == '':
+        break
+    catNames = catNames + [name]
+
+print('The cat names are: ')
+for x in catNames:
+    print('    ' + x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
