ac87d8ce8cecc92cc8b6fcb6fa15a70a4ed8cba8
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-04 22:09:27 +0100

    continuation

--- lists.py
+++ lists.py
@@ -75,6 +75,14 @@ for i in range(len(supplies)):
 print('howdy' in ['hello', 'hi', 'howdy', 'heyas'])
 spam = ['hello', 'hi', 'howdy', 'heyas']
 print('cat' in spam)
+
+myPets = ['Zophie', 'Pooka', 'Fat-tail']
+print('Enter a pet name: ')
+name = input()
+if name not in myPets:
+    print('I do not have pet named ' + name + '.')
+else:
+    print(name + ' is my pet.')
     
 
 
