6ee6dfee563e3626a056e92a7254a3b5a00f789e
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-06 01:20:38 +0100

    continuation

---
+++ commaCode.py
@@ -0,0 +1,15 @@
+spam = ['apples', 'bananas', 'tofu', 'cats']
+
+def convertListToString(argument):
+    convertedArgument = ''
+    for x in argument:
+        if x == argument[len(argument) - 1]:
+            convertedArgument += 'and ' + x
+        else:
+            convertedArgument += x + ', '
+    print(type(convertedArgument))
+    return convertedArgument
+
+print(convertListToString(spam))
+
+    
