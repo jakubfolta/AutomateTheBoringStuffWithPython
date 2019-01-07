8251a5c5dfcf8a03bc3acf57cc89ae169a63609d
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-07 21:50:24 +0100

    continuation

--- characterPictureGrid.py
+++ characterPictureGrid.py
@@ -10,7 +10,8 @@ grid = [['.', '.', '.', '.', '.', '.'],
 
 def printTheImageFromGrid(argument):
     for x in argument:
-        print(argument[0][0])
+        for x in argument:
+            print(argument[0])
 
 
 printTheImageFromGrid(grid)
