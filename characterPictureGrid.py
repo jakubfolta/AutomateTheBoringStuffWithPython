1ac7b6bd32c666312b18595788e57629b2982a71
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-08 20:51:23 +0100

    continuation

--- characterPictureGrid.py
+++ characterPictureGrid.py
@@ -23,6 +23,7 @@ def printPictureFromGrid(argument):
     for x in range(6):
         for y in range(9):
             print(argument[y][x], end = '')
+        print()
 
 
 printPictureFromGrid(grid)
