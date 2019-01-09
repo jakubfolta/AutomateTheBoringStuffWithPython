dc9461159ff233ad11a8ecf50798f2e16df24e83
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-09 21:13:24 +0100

    continuation - small program

---
+++ birthdays.py
@@ -0,0 +1,17 @@
+birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
+
+while True:
+    print('Enter a name to check: (blank to quit)')
+    name = input()
+    if name == '':
+        break
+    if name in birthdays:
+        print(birthdays[name] + 'is the birthaday of ' + name + '.')
+    else:
+        print('I don\'t have birthday info of ' + name + '.')
+        print('What is their birthday?')
+        bday = input()
+        birthdays[name] = bday
+        print('Birthday database updated.')
+
+    exit()
