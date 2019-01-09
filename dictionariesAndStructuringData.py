b1d6cc756b55e0a587fa63c92596b6e5a52fbca9
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-09 20:26:36 +0100

    continuation

--- dictionariesAndStructuringData.py
+++ dictionariesAndStructuringData.py
@@ -2,3 +2,7 @@ myCat = {'size': 'fat', 'color': 'gray', 'disposition': 'loud'}
 print(myCat['size'])
 
 print('My cat has ' + myCat['color'] + ' fur.')
+
+spam = {123: 'Luggage combination', 32: 'The answer'}
+print(spam[123])
+
