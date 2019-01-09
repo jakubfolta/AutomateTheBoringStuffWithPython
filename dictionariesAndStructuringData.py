74f1f86e34f6c031862e565b3faec2c4717ed26d
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-09 20:37:21 +0100

    continuation

--- dictionariesAndStructuringData.py
+++ dictionariesAndStructuringData.py
@@ -5,4 +5,13 @@ print('My cat has ' + myCat['color'] + ' fur.')
 
 spam = {123: 'Luggage combination', 32: 'The answer'}
 print(spam[123])
+#Dictionaries vs Lists
+spam = ['cats', 'dogs', 'moose']
+eggs = ['dogs', 'moose', 'cats']
+print(spam == eggs)
+
+eggs = {'name': 'Zophie', 'species': 'cat'}
+ham = {'species': 'cat', 'name': 'Zophie'}
+print(eggs == ham)
+
 
