3fe971d34e8c1e597126989a74c2f632dcce138e
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-10 20:39:58 +0100

    continuation

--- dictionariesAndStructuringData.py
+++ dictionariesAndStructuringData.py
@@ -49,6 +49,25 @@ print('color' not in spam.keys())
 
 #The get() method
 
+picnicItems = {'apples':5, 'cups':2}
+print('I am bringing ' + str(picnicItems.get('cups', 0)) + ' cups.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
