ce91e23774dab6d9ce6a858adbd39c0a64b026af
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-14 21:42:34 +0100

    continuation

--- theBoard.py
+++ theBoard.py
@@ -1,12 +1,12 @@
-theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
-            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
-            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+theBoard = {'top-L': 'O', 'top-M': '0', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': 'X', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': 'X'}
 
 def printTheBoard(board):
-    print(theBoard['top -L'] + theBoard['top-M'] + theBoard['top-R'])
-    print(-+-+-)
-    print(theBoard['mid -L'] + theBoard['mid-M'] + theBoard['mid-R'])
-    print(-+-+-)
-    print(theBoard['low -L'] + theBoard['low-M'] + theBoard['low-R'])
+    print(theBoard['top-L'] + '|' + theBoard['top-M'] + '|' + theBoard['top-R'])
+    print('-+-+-')
+    print(theBoard['mid-L'] + '|' + theBoard['mid-M'] + '|' + theBoard['mid-R'])
+    print('-+-+-')
+    print(theBoard['low-L'] + theBoard['low-M'] + theBoard['low-R'])
 
 print(printTheBoard(theBoard))
