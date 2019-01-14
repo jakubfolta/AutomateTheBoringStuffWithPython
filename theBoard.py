e5f97e2d5b7db02eba32b93a287be937c7827dab
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-14 22:17:19 +0100

    continuation

--- theBoard.py
+++ theBoard.py
@@ -2,20 +2,22 @@ theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
             'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 
-def printTheBoard(board):
+def printTheBoard(dictionary):
     print(theBoard['top-L'] + '|' + theBoard['top-M'] + '|' + theBoard['top-R'])
     print('-+-+-')
     print(theBoard['mid-L'] + '|' + theBoard['mid-M'] + '|' + theBoard['mid-R'])
     print('-+-+-')
-    print(theBoard['low-L'] + theBoard['low-M'] + theBoard['low-R'])
-    print('+|+|+')
+    print(theBoard['low-L'] + '|' + theBoard['low-M'] + '|' + theBoard['low-R'])
 
 turn = 'X'
 for x in range(9):
     printTheBoard(theBoard)
-    print('Turn for ' + turn + '. Move on which space')
+    print('Turn for ' + turn + '. Move on which space?')
     move = input()
     theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
     
-
-
+printTheBoard(theBoard)
