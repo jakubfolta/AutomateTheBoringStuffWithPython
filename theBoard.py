91b917acbbd2e610ab287a44b26ba06019e004a8
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-14 21:37:14 +0100

    continuation

--- theBoard.py
+++ theBoard.py
@@ -6,6 +6,7 @@ def printTheBoard(board):
     print(theBoard['top -L'] + theBoard['top-M'] + theBoard['top-R'])
     print(-+-+-)
     print(theBoard['mid -L'] + theBoard['mid-M'] + theBoard['mid-R'])
-
+    print(-+-+-)
+    print(theBoard['low -L'] + theBoard['low-M'] + theBoard['low-R'])
 
 print(printTheBoard(theBoard))
