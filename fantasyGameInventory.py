323a7c254d1fd61b2ec3c2e67dbafb15d5a71cf6
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-15 22:24:04 +0100

    continuation

--- fantasyGameInventory.py
+++ fantasyGameInventory.py
@@ -1,2 +1,10 @@
-def displayInventory():
-    pass
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def displayInventory(inventory):
+    print('Inventory: ')
+    totalItems = 0
+    for k, v in inventory.items():
+        totalItems = totalItems + v
+    print(totalItems)
+
+displayInventory(stuff)
