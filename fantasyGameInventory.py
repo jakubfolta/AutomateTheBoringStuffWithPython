a02706188a7aa2d7f69132e425de1327457630d6
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-21 17:16:05 +0100

    continuation

--- fantasyGameInventory.py
+++ fantasyGameInventory.py
@@ -1,21 +1,9 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
 def displayInventory(inventory):
-    print('Inventory: ')
-    totalItems = 0
-    for k, v in inventory.items():
-        print(str(v) + ' ' + k)
-        totalItems = totalItems + v
-    print('Total number of items: ' + str(totalItems))
-
-displayInventory(stuff)
-
 
 def addToInventory(inventory, addedItems):
-    for x in addedItems:
-        inventory.setdefault(x, 0)
-        inventory[x] += 1
-    return inventory
+
         
 
 inv = {'gold coin': 42, 'rope': 1}
@@ -28,3 +16,35 @@ displayInventory(inv)
         
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    print('Inventory: ')
+    totalItems = 0
+    for k, v in inventory.items():
+        print(str(v) + ' ' + k)
+        totalItems = totalItems + v
+    print('Total number of items: ' + str(totalItems))
+
+displayInventory(stuff)
+
+
+
+    for x in addedItems:
+        inventory.setdefault(x, 0)
+        inventory[x] += 1
+    return inventory
