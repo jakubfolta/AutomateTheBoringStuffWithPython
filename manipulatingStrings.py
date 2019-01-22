de3cdb652b745c381b8f1c3918112012e17cba20
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-22 16:57:45 +0100

    continuation

--- manipulatingStrings.py
+++ manipulatingStrings.py
@@ -9,3 +9,14 @@ spam = 'Say hi to Bob\'s mother'
 print(spam)
 
 print('Hello there!\nHow are you?\nI\'m doing fine.')
+
+#Raw strings
+
+print(r'That is Carol\'s cat.')
+
+#Multiline strings with triple quotes
+
+print('''Dear Alice,
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+Sincerely,
+Bob''')
