4b9c79148d85c183cd3e36b3da5712af28f4c4fe
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-23 13:38:59 +0100

    continuation

--- manipulatingStrings.py
+++ manipulatingStrings.py
@@ -102,10 +102,23 @@ print(', '.join(['cats', 'rats', 'bats']))
 print('ABC'.join(['My', 'name', 'is', 'Simon.']))
 
 print('My name is Simon.'.split())
+print('MyABCnameABCisABCSimon.'.split('ABC'))
+print('My name is Simon'.split('m'))
 
+spam = '''Dear Alice,
+How have you been? I am fine.
+There is a container in the fridge
+that is labeled "Milk Experiment".
 
+Please do not drink it.
+Sincerely,
+Bob'''
+print(spam.split('\n'))
 
+#Justifying text with rjust(), ljust(), and center()
 
+print('Hello'.rjust(10))
+print('"Hello"'.ljust(20))
 
 
 
