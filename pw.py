8bf3056bcc0c5996769402b06d378bc7044d098d
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-25 18:54:48 +0100

    continuation

--- pw.py
+++ pw.py
@@ -4,13 +4,20 @@
 #! python 3
 # pw.py - An insecure password locker program
 
-Passwords = {'email': 'fdwfrevrvfd',
+PASSWORDS = {'email': 'fdwfrevrvfd',
              'blog': 'fwrfervkn3475ygvnf',
              'luggage': 'weih78238dehw8'}
 
-import sys
+import sys, pyperclip
 if len (sys.argv) < 2:
     print('Usage: python pw.py [account] - copy account password')
     sys.exit()
 
 account = sys.argv[1] # first command line arg is the account name
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + 'copied to clipboard.')
+else:
+    print('There is no account named ' + account + '.')
+    
