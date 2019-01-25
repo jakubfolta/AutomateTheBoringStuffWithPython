3b9d5882eab690ca0b0ea5a4897c0c9847a56f1e
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-25 19:03:57 +0100

    continuation

--- pw.py
+++ pw.py
@@ -1,7 +1,7 @@
+#! python 3
 #Project: password locker
 #Step 1: Program design and data structures
 
-#! python 3
 # pw.py - An insecure password locker program
 
 PASSWORDS = {'email': 'fdwfrevrvfd',
