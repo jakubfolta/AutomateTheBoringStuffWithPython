fa2fe01e5877392599b1b291986bf284227d9cf4
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-27 17:14:35 +0100

    continuation

---
+++ tablePrinter.py
@@ -0,0 +1,10 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+def printTable():
+    
+
+
+
+printTable()
