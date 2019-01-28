d3b15aff28a919ccd08b70916dd456af78bbd381
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-28 18:12:58 +0100

    solidifying knowledge

--- commaCode.py
+++ commaCode.py
@@ -2,12 +2,26 @@ spam = ['apples', 'bananas', 5, 'tofu', [3, 4], 'cats', 2]
 
 def convertListToString(argument):
     convertedArgument = ''
+
     for x in argument:
         if x == argument[len(argument) - 1]:
             convertedArgument += 'and ' + str(x)
         else:
             convertedArgument += str(x) + ', '
     return convertedArgument
+
+print(convertListToString(spam))
+
+
+def convertListToString(argument):
+    phrase = ''
+
+    for x in argument:
+        if x == argument[len(argument) - 1]:
+            phrase += 'and' + str(x) + '.'
+        else:
+            phrase += str(x) + ', '
+    return phrase
 
 print(convertListToString(spam))
 
