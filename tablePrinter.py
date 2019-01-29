5afae8d3481ff112c45c7841da69175f1e941f6c
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-29 17:52:32 +0100

    continuation

--- tablePrinter.py
+++ tablePrinter.py
@@ -2,14 +2,17 @@ tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
 
+def findLongestString(table):
+
+
 def printTable(table):
-    colNumb = [0] * len(table)
+    colWidth = [0] * len(table)
     cols = len(tableData[0])
     rows = len(tableData)
 
     for x in range(cols):
         for y in range(rows):
-            print((table[y][x]).ljust(3), end = ' ')
+            print((table[y][x]).rjust(8), end = ' ')
         print()
             
     print(colNumb)
