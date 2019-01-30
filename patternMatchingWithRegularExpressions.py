67e23bc35e8bb72a2c0dd69dd5d1b2c9a0af329e
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-30 21:23:18 +0100

    continuation

--- patternMatchingWithRegularExpressions.py
+++ patternMatchingWithRegularExpressions.py
@@ -56,9 +56,20 @@ print(mo.group(1))
 print(mo.group(2))
 print('Area code is: ' + mo.group(1))
 print(mo.group(0))
+print(mo.group())
 
+print(mo.groups())
 
+areaCode, mainNumber = mo.groups()
+print(areaCode)
+print(mainNumber)
 
+phoneNumRegex = re.compile(r'(\(\d\d\d\)) (\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search('My phone number is (415) 555-5465.')
+print(mo.group(1))
+print(mo.group(2))
+
+#Matching multiple groups with the pipe
 
 
 
