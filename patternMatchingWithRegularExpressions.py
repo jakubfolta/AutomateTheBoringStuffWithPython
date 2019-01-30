458687fa6d4a1bc667cceaa72cd0d7616987403b
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-30 18:10:27 +0100

    continuation

--- patternMatchingWithRegularExpressions.py
+++ patternMatchingWithRegularExpressions.py
@@ -1,1 +1,21 @@
 #Finding patterns of text without regular expressions
+
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    for i in range(3, 7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != '-':
+        return False
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False
+    return True
+        
+        
