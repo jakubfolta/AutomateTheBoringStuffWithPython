20ba9a8e14fbf781ee154a1f5fb4659cd0fb7686
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-01 21:39:47 +0100

    continuation

--- patternMatchingWithRegularExpressions.py
+++ patternMatchingWithRegularExpressions.py
@@ -81,5 +81,11 @@ print(mo2.group())
 
 batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
 mo = batRegex.search('Batmobile lost a wheel')
-mo.group()
+
+print(mo.group())
+
+print(mo.group(1))
+
+#Optional matching with a question mark
+
 
