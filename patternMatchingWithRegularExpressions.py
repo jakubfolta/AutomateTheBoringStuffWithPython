0f4c2eb645b894dc590faba28c5a7b0fb3c62ed9
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-01 21:37:31 +0100

    continuation

--- patternMatchingWithRegularExpressions.py
+++ patternMatchingWithRegularExpressions.py
@@ -78,3 +78,8 @@ print(mo1.group())
 mo2 = heroRegex.search('Tina Fey and Batman')
 
 print(mo2.group())
+
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobile lost a wheel')
+mo.group()
+
