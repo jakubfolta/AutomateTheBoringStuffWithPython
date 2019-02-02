d4e1cb387f9f60e995b85d645aa0a8d361ded82f
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-02 15:26:52 +0100

    continuation

--- patternMatchingWithRegularExpressions.py
+++ patternMatchingWithRegularExpressions.py
@@ -113,7 +113,9 @@ mo1 = batRegex.search('The adventures of Batman')
 
 print(mo1.group())
 
-mo2 = batRegex.search()
+mo2 = batRegex.search('The adventures of Batwoman')
+
+print(mo2.group())
 
 
 
