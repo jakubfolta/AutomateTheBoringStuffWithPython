14261a6b33441b93afaba19c7b20736b3c03ac58
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-03 11:18:19 +0100

    continuation

--- patternMatchingWithRegularExpressions.py
+++ patternMatchingWithRegularExpressions.py
@@ -177,6 +177,11 @@ mo = xmasRegex.findall('12 drummers, 11 pipers, \
 7 swans, 6 geese, 5 rings, 4 birds, 3 hens, 2 doves, 1 partridge')
 print(mo)
 
+#Making your own character classes
+
+vowelRegex = re.compile(r'[aeiouAEIOU]')
+mo = vowelRegex.findall('Robocop eats baby food. BABY FOOD.')
+print('-'.join(mo))
 
 
 
