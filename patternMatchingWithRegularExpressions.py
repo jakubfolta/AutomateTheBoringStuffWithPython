7f7005d5c1a61609b18fc17c9f99282b9b95cc27
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-05 19:34:35 +0100

    continuation

--- patternMatchingWithRegularExpressions.py
+++ patternMatchingWithRegularExpressions.py
@@ -253,6 +253,22 @@ namesRegex = re.compile(r'Agent \w+')
 mo = namesRegex.sub('CENSORED', 'Agent Alice gave the secret documents to Agent Bob.')
 print(mo)
 
+agentNamesRegex = re.compile(r'Agent (\w)\w*')
+mo = agentNamesRegex.sub(r'\1****', 'Agent Alice told Agent Carol \
+that Agent Eve knew Agent Bob was a double Agent.')
+print(mo)
+
+#Managing complex regexes
+
+phoneRegex = re.compile(r'''(
+(\d{3}|\(\d{3}\))?             #area code
+(\s|-|\.)?                     #separator
+\d{3}                          #first 3 digits
+(\s|-|\.)                      #separator
+\d{4}                          #last 4 digits
+(\s*(ext|x|ext.)\s*\d{2,5})?   #extension
+)''', re.VERBOSE)
+
 
 
 
