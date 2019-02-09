3d1f4585bf9e7139375fdad2a97abee3e83469d0
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-09 14:06:09 +0100

    continuation

--- patternMatchingWithRegularExpressions.py
+++ patternMatchingWithRegularExpressions.py
@@ -303,3 +303,13 @@ mo = textRegex.search('Alice eats apples.')
 mor = textRegex.search('ALICE THROWS FOOTBALLS.')
 print(mo.group())
 print(mor)
+
+#Q: Write a function that uses regular expressions to make sure the password
+#string it is passed is strong. A strong password is defined as one that
+#is at least eight characters long, contains both uppercase and
+#lowercase characters, and has at least one digit. You may need to test
+#the string against multiple regex patterns to validate its strength.
+
+def checkIfStrongPassword(text):
+    passwordRegex = re.compile(r'[a-z]{8}')
+
