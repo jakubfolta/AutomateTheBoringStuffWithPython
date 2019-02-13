a03afa5899eb70bee0a958ff37cb36e9e3912dad
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-13 16:26:03 +0100

    continuation

--- patternMatchingWithRegularExpressions.py
+++ patternMatchingWithRegularExpressions.py
@@ -331,4 +331,37 @@ character and at least one digit.   ')
 
 checkIfStrongPassword()
 
-#Q: 
+#Q: Write a function that takes a string and does the same
+#thing as the strip() string method. If no other arguments
+#are passed other than the string to strip, then whitespace
+#characters will be removed from the beginning and end of
+#the string. Otherwise, the characters specified in the second
+#argument to the function will be removed from the string.
+
+stripRegex = re.compile(r'')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
