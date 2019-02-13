952ba79bfab0a2edd227ddfb1477b353b1206ad8
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-13 16:43:06 +0100

    continuation

--- patternMatchingWithRegularExpressions.py
+++ patternMatchingWithRegularExpressions.py
@@ -338,7 +338,9 @@ checkIfStrongPassword()
 #the string. Otherwise, the characters specified in the second
 #argument to the function will be removed from the string.
 
-stripRegex = re.compile(r'[a-zA-Z0-9]*')
+stripRegex = re.compile(r'\s*')
+mo = stripRegex.sub('', '" kjghgj "')
+print(mo)
 
 
 
