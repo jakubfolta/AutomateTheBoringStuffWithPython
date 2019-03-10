8e200d87d5f5af5a77cb5207991eef12ef76982b
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-10 13:06:20 +0100

    continuation

--- organizing_files.py
+++ organizing_files.py
@@ -5,7 +5,12 @@ os.chdir('C:\\')
 #shutil.copy('C:\\BnetLog.txt', 'C:\\delicious')
 #shutil.copy('C:\\BnetLog.txt', 'C:\\delicious\spam.txt')
 
-#shutil.copytree('C:\\New folder', 'C:\\New folder backup')
+# shutil.copytree('C:\\New folder', 'C:\\New folder backup')
+#
+# shutil.move('C:\\BnetLog.txt', 'C:\\New folder backup2')
+# shutil.move('C:\\New folder backup2', 'C:\\backup')
 
-#shutil.move('C:\\BnetLog.txt', 'C:\\New folder backup2')
-shutil.move('C:\\New folder backup2', 'C:\\backup')
+# Permanently deleting files and folders
+#os.unlink('C:\\backup')
+#os.rmdir('C:\\New f')
+shutil.rmtree('C:\\New folder')
