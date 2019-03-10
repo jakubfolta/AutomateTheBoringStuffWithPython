a0c8f0c7cd1daa8874b8acd1c22aed5bf6002005
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-10 12:48:47 +0100

    continuation

--- organizing_files.py
+++ organizing_files.py
@@ -2,5 +2,10 @@ import shutil
 import os
 
 os.chdir('C:\\')
-shutil.copy('C:\\BnetLog.txt', 'C:\\delicious')
-shutil.copy('C:\\BnetLog.txt', 'C:\\delicious\spam.txt')
+#shutil.copy('C:\\BnetLog.txt', 'C:\\delicious')
+#shutil.copy('C:\\BnetLog.txt', 'C:\\delicious\spam.txt')
+
+#shutil.copytree('C:\\New folder', 'C:\\New folder backup')
+
+#shutil.move('C:\\BnetLog.txt', 'C:\\New folder backup2')
+shutil.move('C:\\New folder backup2', 'C:\\backup')
