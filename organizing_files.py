f510629840d553f6b957d5d4a7a748df1f961ea9
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-11 14:53:56 +0100

    continuation

--- organizing_files.py
+++ organizing_files.py
@@ -1,6 +1,7 @@
 import shutil
 import os
 import send2trash
+import zipfile
 
 os.chdir('C:\\')
 #shutil.copy('C:\\BnetLog.txt', 'C:\\delicious')
@@ -19,4 +20,20 @@ os.chdir('C:\\Users\ogi-8\Desktop\PythonProjects')
 with open('file.txt', 'a') as bacon_file:
     bacon_file.write('Bacon isn\'t a vegetable.')
 
+
 send2trash.send2trash('file.txt')
+#Walking a directory tree
+
+ '''Reading Zip Files'''
+
+example_zip = zipfile.ZipFile('ToDoList.zip')
+print(example_zip.namelist())
+
+spam_info = example_zip.getinfo('ToDoList/toDo.py')
+print(spam_info.file_size)
+print(spam_info.compress_size)
+
+print('Compressed file is {} smaller!'.format(round(spam_info.file_size / spam_info.compress_size, 2)))
+example_zip.close()
+
+# Extracting from ZIP files
