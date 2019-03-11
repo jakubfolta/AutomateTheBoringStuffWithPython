69fa8d9a5a70a5969d29cb12bcc9bf8aebaf0077
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-11 22:50:56 +0100

    continuation

--- rename_dates.py
+++ rename_dates.py
@@ -7,7 +7,21 @@ import re
 import shutil
 
 # TODO: Create regex to find files with American date format.
+date_pattern = re.compile(r'''^(.*?) # all text before the date
+    ((0|1)?\d)-                      # one or two digits for the month
+    ((0|1|2|3)?\d)-                  # one or two for day
+    ((19|20)\d\d)                    # four digits for year
+    (.*?)$                           # all text after the date
+''', re.VERBOSE)
 
 # TODO: Loop over the files in the working directoryself.
 
 # TODO: Skip files without date.
+
+# TODO: Get the different parts of the filename.
+
+# TODO: Form the European-style filename.
+
+# TODO: Get the full, absolute file paths.
+
+# TODO: Rename the files.
