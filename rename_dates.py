3a1990806a61a1ed72b06ac017e239e8e0c0b9c2
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-11 22:11:21 +0100

    continuation

---
+++ rename_dates.py
@@ -0,0 +1,13 @@
+#! python3
+
+# rename_dates.py - Rename filenames with American MM-DD-YYYY date format to European DD-MM-YYYY.
+
+import os
+import re
+import shutil
+
+# TODO: Create regex to find files with American date format.
+
+# TODO: Loop over the files in the working directoryself.
+
+# TODO: Skip files without date.
