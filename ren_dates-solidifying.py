b9d0f411f79fd1e01ca5eef7d8df4c6b340580e2
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-13 13:32:31 +0100

    continuation

---
+++ ren_dates-solidifying.py
@@ -0,0 +1,12 @@
+#! python3
+
+# ren_dates-solidifying.py - program which change filenames with european date format to american date format.
+
+# TODO: Import modules.
+# TODO: Create regex to find european date format.
+# TODO: Loop the files in the working directory.
+# TODO: Skip files without dates.
+# TODO: Select parts of the date.
+# TODO: Create proper filename.
+# TODO: Create absolute paths to exchange.
+# TODO: Change filenames and print changes.
