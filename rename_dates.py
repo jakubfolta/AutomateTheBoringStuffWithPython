0e9b29d35f482c70826933bb90ba1f3c23fa5773
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-13 09:28:58 +0100

    continuation

--- rename_dates.py
+++ rename_dates.py
@@ -14,6 +14,12 @@ date_pattern = re.compile(r'''^(.*?) # all text before the date
     (.*?)$                           # all text after the date
 ''', re.VERBOSE)
 
+date_pattern = re.compile(r'''^(.*?) # all text before the date
+    ((0|1)?\d)-                      # one or two digits for month
+    ((0|1|2|3)?\d)-                  # one or two digits for the day
+    ((19|20)\d\d)                    # four digits for the year
+    ((.*?)$)                         # all text after the date
+''', re.VERBOSE)
 # TODO: Loop over the files in the working directory.
 for amer_filename in os.listdir('.'):
     mo = date_pattern.search(amer_filename)
