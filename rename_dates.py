ea3d5d10a176565ff479037a7afbc72607a5d312
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-13 13:14:00 +0100

    continuation

--- rename_dates.py
+++ rename_dates.py
@@ -14,32 +14,29 @@ date_pattern = re.compile(r'''^(.*?) # all text before the date
     (.*?)$                           # all text after the date
 ''', re.VERBOSE)
 
-# TODO: Loop over the files in the working directory.
+# Loop over the files in the working directory.
 for amer_filename in os.listdir('.'):
     mo = date_pattern.search(amer_filename)
-print(os.path.abspath('.'))
-print(os.getcwd())
-'''
-# TODO: Skip files without date.
+
+# Skip files without date.
     if mo == None:
         continue
 
-# TODO: Get the different parts of the filename.
-    befor_part = mo.group(1)
+# Get the different parts of the filename.
+    before_part = mo.group(1)
     month_part = mo.group(2)
-    day_part = mo.group(3)
-    year_part = mo.group(4)
-    after_part = mo.group(5)
+    day_part = mo.group(4)
+    year_part = mo.group(6)
+    after_part = mo.group(8)
 
-# TODO: Form the European-style filename.
+# Form the European-style filename.
     euro_filename = before_part + day_part + '-' + month_part + '-' + year_part + after_part
 
-# TODO: Get the full, absolute file paths.
+# Get the full, absolute file paths.
     abs_working_directory = os.path.abspath('.')
     amer_filename = os.path.join(abs_working_directory, amer_filename)
     euro_filename = os.path.join(abs_working_directory, euro_filename)
 
-# TODO: Rename the files.
+# Rename the files.
     print('Renaming "{}" to "{}"...'.format(amer_filename, euro_filename))
-    #shutil.move(amer_filename, euro_filename) # uncomment after testing
-'''
+    shutil.move(amer_filename, euro_filename) # uncomment after testing
