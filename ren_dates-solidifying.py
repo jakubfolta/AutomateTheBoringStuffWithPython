a25d7a3c516fb7effa41b5603aeb31d830740359
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-14 09:53:13 +0100

    continuation

--- ren_dates-solidifying.py
+++ ren_dates-solidifying.py
@@ -5,7 +5,7 @@
 # TODO: Import modules.
 import os
 import re
-import shutile
+import shutil
 
 # TODO: Create regex to find european date format.
 date_pattern = re.compile(r'''^(.*?) # all text before date
@@ -20,7 +20,7 @@ for europ_filename in os.listdir('.'):
     match = date_pattern.search(europ_filename)
 
 # TODO: Skip files without dates.
-    if gr == None:
+    if match == None:
         continue
 
 # TODO: Select parts of the date.
@@ -34,4 +34,10 @@ for europ_filename in os.listdir('.'):
     amer_filename = before_text + month_match + '-' + day_match + '-' + year_match + after_text
 
 # TODO: Create absolute paths to exchange.
+    abs_path = os.path.abspath('.')
+    europ_filename = os.path.join(abs_path, europ_filename)
+    amer_filename = os.path.join(abs_path, amer_filename)
+
 # TODO: Change filenames and print changes.
+    print('Change "{}" to "{}"'.format(europ_filename, amer_filename))
+    #shutil.move(europ_filename, amer_filename)
