90a8549d56a637e7de20a8ec095e1d0b14b35920
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-14 10:02:24 +0100

    continuation

---
+++ rename_dates-solidifying.py
@@ -0,0 +1,15 @@
+#! python3
+
+# rename_dates.py - Rename filenames with american date format to european date format.
+
+# TODO: Import essential modules.
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
