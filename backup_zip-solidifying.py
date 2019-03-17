47a3f780ae9bfefae4592cbe02ac7e64eb9abfc0
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-17 13:43:28 +0100

    continuation

---
+++ backup_zip-solidifying.py
@@ -0,0 +1,13 @@
+#! python3
+
+# backup_zip-solidifying.py - Copies an entire folder and its contents into
+# a ZIP file whose filename increments.
+
+import zipfile
+import os
+
+# TODO: Create function.
+# TODO: Create abspath for folder path.
+# TODO: Create zipfile name.
+# TODO: Create zipfile.
+# TODO: Loop through pathtree and add to zipfile folders and files.
