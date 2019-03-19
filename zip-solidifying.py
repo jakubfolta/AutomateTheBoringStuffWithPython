cd58b034156009f77b280d4205459a3ff262ad8b
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-19 14:03:05 +0100

    continuation

--- zip-solidifying.py
+++ zip-solidifying.py
@@ -3,10 +3,25 @@
 # zip-solidifying.py -  Copies an entire folder and its contents into
 # a ZIP file whose filename increments.
 
-# TODO: Create function.
-# TODO: Set abspath to variable.
-# TODO: Create zipfile name with while loop.
+
+# Import essential modules.
+import os
+import zipfile
+
+# Create function.
+def backup_folder(folder):
+# Set abspath to variable.
+    abspath = os.path.abspath(folder)
+
+# Create zipfile name with while loop.
+    number = 1
+    while True:
+        zip_name = os.path.basename(abspath) + '_' + '.zip'
+        if not os.path.exists(zip_name):
+            break
+        number += 1
+
 # TODO: Create zipfile.
 # TODO: Use for loop and os.walk() to add current folder and its files to zipfile.
-# TODO:
-# TODO:
+# TODO: Close zipfile.
+# TODO: Call function.
