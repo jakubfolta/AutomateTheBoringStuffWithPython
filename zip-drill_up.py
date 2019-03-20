d4d625ca7e788d4de222b2e4874efc44a622b07f
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-20 22:59:45 +0100

    continuation

---
+++ zip-drill_up.py
@@ -0,0 +1,14 @@
+#! python3
+
+# zip-drill_up.py - Copies an entire folder and its contents into
+# a ZIP file whose filename increments.
+
+# TODO: Create a function and set abs path to argument.
+def create_backup(folder):
+    abspath_folder = os.path.abspath(folder)
+
+# TODO: Create zip file name in a while loop.
+# TODO: Create zip file.
+# TODO: Loop through argument path and add folders and files to zip file.
+# TODO: Close zip file.
+# TODO: Call function.
