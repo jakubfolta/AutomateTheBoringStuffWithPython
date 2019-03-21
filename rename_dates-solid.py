f689d818b52aa52e6d9c5aefc79c3c60cd50beb3
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-21 21:10:18 +0100

    continuation

---
+++ rename_dates-solid.py
@@ -0,0 +1,12 @@
+#! python3
+
+# rename_dates-solid.py - program which change filenames with european date format to american date format.
+
+# TODO: Import essential modules.
+# TODO: Create regex to find european date format.
+# TODO: 
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
