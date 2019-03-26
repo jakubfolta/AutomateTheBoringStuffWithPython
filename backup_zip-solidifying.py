4a84bbd8b32b3671fde80cb68b863f6c15b296ba
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-26 16:40:46 +0100

    continuation

--- backup_zip-solidifying.py
+++ backup_zip-solidifying.py
@@ -26,16 +26,18 @@ def create_zipfile(folder):
 
 # Loop through pathtree and add to zipfile folders and files.
     for folder, subfolder, filename in os.walk(folder):
+
 # Add folder to zip_file.
         print('Adding files in {}.'.format(folder))
         zip_file.write(folder)
+
 # Add files.
         for file in filename:
             base_name = os.path.basename(folder) + '_'
             if file.startswith(base_name) and file.endswith('.zip'):
                 continue
-            zip_file.write(os.path.join(folder, file))
+                zip_file.write(os.path.join(folder, file))
     zip_file.close()
     print('Done')
 
-create_zipfile('C:\\Users\ogi-8\Desktop\PythonProjects\AddDigits')
+create_zipfile(r'C:\Users\ogi-8\Desktop\PythonProjects\AddDigits')
