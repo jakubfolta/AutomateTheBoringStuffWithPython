39d96f5894c6c4c314395a3443f811477586efcf
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-05 19:07:50 +0200

    solidifying knowledge

--- zip_file-solidifying.py
+++ zip_file-solidifying.py
@@ -3,4 +3,12 @@
 '''zip_file-solidifying.py - Copies an entire folder and its contents into
  a ZIP file whose filename increments.'''
 
- 
+# TODO: Import essential modules.
+# TODO: Create function.
+# TODO: Create basename.
+# TODO: Create new filename.
+# TODO: Create zipfile.
+# TODO: Walk through directory tree in for loop.
+# TODO: Add folder to zipfile.
+# TODO: Add files to zipfile.
+# TODO: Close zipfile.
