57dd1612f52f692caef7e22d8f49a4339e9ac89e
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-05 14:39:06 +0200

    solidifying knowledge

--- backup-solidifying.py
+++ backup-solidifying.py
@@ -3,10 +3,10 @@
 '''backup_zip-solidifying.py - Copies an entire folder and its contents into
  a ZIP file whose filename increments.'''
 
-# TODO: 
-# TODO:
-# TODO:
-# TODO:
+# TODO: Import essential modules.
+# TODO: Set directory to check.
+# TODO: Walk through directory with for loop and os.walk().
+# TODO: Set new filename for backup.
 # TODO:
 # TODO:
 # TODO:
