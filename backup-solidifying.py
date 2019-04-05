c0630a41b70aa4c9ae17bfd529334637dbe126ba
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-05 14:35:33 +0200

    solidifying knowledge

--- backup-solidifying.py
+++ backup-solidifying.py
@@ -1,1 +1,13 @@
 #! python3
+
+'''backup_zip-solidifying.py - Copies an entire folder and its contents into
+ a ZIP file whose filename increments.'''
+
+# TODO: 
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
