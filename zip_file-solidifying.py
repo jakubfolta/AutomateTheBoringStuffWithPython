c4cd8bc5d1540792a00f2ac9addca808f491db85
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-05 19:20:03 +0200

    solidifying knowledge

--- zip_file-solidifying.py
+++ zip_file-solidifying.py
@@ -13,7 +13,7 @@ def create_zipfile(directory):
 # Create basename.
     base_name = os.path.basename(directory)
 
-# TODO: Create new filename.
+# Create new filename.
     num = 1
     while True:
         zip_name = base_name + '_' + str(num) + '.zip'
@@ -21,11 +21,22 @@ def create_zipfile(directory):
             break
         num += 1
 
+# Create zipfile.
+    print('Creating "{}"'.format(zip_name))
+    zip_file = zipfile.ZipFile(zip_name, 'w')
 
-# TODO: Create zipfile.
 # TODO: Walk through directory tree in for loop.
+    for folder, subfolder, files in os.walk(directory):
+
 # TODO: Add folder to zipfile.
+        print('Adding files in "{}"'.format(folder))
+        zip_file.write(folder)
+        print('Adding:')
 # TODO: Add files to zipfile.
+        for file in files:
+            if file.endswith('.zip'):
+                continue
+            print(file)
 # TODO: Close zipfile.
 
 create_zipfile(r'C:\Users\ogi-8\Desktop\PythonProjects\AddDigits\Sample_zip')
