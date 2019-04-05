6793d56b00da9d71f1c2dd37ef8614e20fcaa8ec
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-05 19:14:07 +0200

    solidifying knowledge

--- zip_file-solidifying.py
+++ zip_file-solidifying.py
@@ -3,12 +3,29 @@
 '''zip_file-solidifying.py - Copies an entire folder and its contents into
  a ZIP file whose filename increments.'''
 
-# TODO: Import essential modules.
-# TODO: Create function.
-# TODO: Create basename.
+# Import essential modules.
+import os
+import zipfile
+
+# Create function.
+def create_zipfile(directory):
+
+# Create basename.
+    base_name = os.path.basename(directory)
+
 # TODO: Create new filename.
+    num = 1
+    while True:
+        zip_name = base_name + '_' + str(num) + '.zip'
+        if not os.path.exists(zip_name):
+            break
+        num += 1
+
+
 # TODO: Create zipfile.
 # TODO: Walk through directory tree in for loop.
 # TODO: Add folder to zipfile.
 # TODO: Add files to zipfile.
 # TODO: Close zipfile.
+
+create_zipfile(r'C:\Users\ogi-8\Desktop\PythonProjects\AddDigits\Sample_zip')
