2a7276ea73ebf8b708de5e33fd45640536c24f1a
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-07 18:11:27 +0200

    solidifying knowledge

--- ren_filenames-solidifying.py
+++ ren_filenames-solidifying.py
@@ -3,9 +3,10 @@
 '''ren_filenames-solidifying.py - Rename filenames with american date format to european date format.'''
 
 # TODO: Import essential modules.
-# TODO: 
+# TODO: Create date format regex.
+# TODO: Create basename.
+# TODO: Loop through files in specified directory with os.listdir().
+# TODO: Set matched groups.
 # TODO:
-# TODO:
-# TODO:
-# TODO:
+# TODO: Change filename to proper date format.
 # TODO:
