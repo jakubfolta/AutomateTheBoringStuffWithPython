ea500bedbfc4e1fe5f64827e4b1c02c3a470a669
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-07 18:05:17 +0200

    solidifying knowledge

---
+++ ren_filenames-solidifying.py
@@ -0,0 +1,11 @@
+#! python3
+
+'''ren_filenames-solidifying.py - Rename filenames with american date format to european date format.'''
+
+# TODO: Import essential modules.
+# TODO: 
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
