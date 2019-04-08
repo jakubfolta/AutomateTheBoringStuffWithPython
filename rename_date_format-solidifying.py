462cc80ab2c2274f4c2c9d24ba7893a3f8e208c3
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-08 19:12:06 +0200

    solidifying knowledge

--- rename_date_format-solidifying.py
+++ rename_date_format-solidifying.py
@@ -7,10 +7,17 @@ import os
 import re
 import shutil
 
-# TODO: Create american date format regex.
-
+# Create american date format regex.
+amer_date_format = re.compile(r'''^(.*?)    # all text before date
+    ((0|1)?\d)-                             # one or two digits for a month
+    ((0|1|2|3)?\d)-                         # one or two digits for a day
+    ((19|20)\d\d)                           # four digits for a year
+    (.*?)$                                  # all text after date
+''', re.VERBOSE)
 
 # TODO: Set directory to check.
+
+
 # TODO: Create for loop and use os.listdir() to check files inside directory.
 # TODO: Get groups from matches.
 # TODO: Create european date format from matched groups.
