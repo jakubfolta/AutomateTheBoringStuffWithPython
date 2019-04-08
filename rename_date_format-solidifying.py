ef8dd5b5271b0a84ddf159940b9fb35b48bf4f4e
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-08 19:00:33 +0200

    solidifying knowledge

---
+++ rename_date_format-solidifying.py
@@ -0,0 +1,12 @@
+#! python3
+
+'''rename-date-format-solidifying.py - Rename filenames with american date format to european date format.'''
+
+# TODO: Import essential modules.
+# TODO: Create american date format regex.
+# TODO: Set directory to check.
+# TODO: Create for loop and use os.listdir() to check files inside directory.
+# TODO: Get groups from matches.
+# TODO: Create european date format from matched groups.
+# TODO: Set proper absolute paths to exchange.
+# TODO: Print and change the filename using shutil module.
