abf253a21958d89c6b0d5a8aedb4ac886f8c0bd2
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-13 10:00:30 +0200

    continuation

--- box_print.py
+++ box_print.py
@@ -2,4 +2,10 @@ def box_print(symbol, width, height):
     if len(symbol) != 1:
         raise Exception('Symbol must be a single character string.')
     if width <= 2:
-        raise Exception('')
+        raise Exception('Width must be greater than 2.')
+    if height <= 2:
+        raise Exception('Height must be greater than 2.')
+    print(symbol * width)
+    for i in range(height - 2):
+        print(symbol + (' ' * (width - 2)) + symbol)
+    print(symbol * width)
