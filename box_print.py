716b33b2bbb3f43eaa9a8970e218640bf0828652
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-13 10:04:04 +0200

    continuation

--- box_print.py
+++ box_print.py
@@ -9,3 +9,9 @@ def box_print(symbol, width, height):
     for i in range(height - 2):
         print(symbol + (' ' * (width - 2)) + symbol)
     print(symbol * width)
+
+for sym, w, h in (('*', 4, 4), ('O', 20, 5), ('x', 1, 3), ('ZZ', 3, 3)):
+    try:
+        box_print(sym, w, h)
+    except:
+        
