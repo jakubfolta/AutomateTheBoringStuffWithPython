04427c0d7f1793176f910e0efa8fd82d8b07c170
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-14 10:55:27 +0200

    continuation

--- error_example.py
+++ error_example.py
@@ -7,8 +7,6 @@ import traceback
 try:
     raise Exception('This is the error message!')
 except:
-    with 
-    error_file = open('error_info.txt', 'w')
-    error_file.write(traceback.format_exc())
-    error_file.close()
+    with open('error_info.txt', 'w') as error_file:
+        error_file.write(traceback.format_exc())
     print('The traceback info was written to error_info.txt')
