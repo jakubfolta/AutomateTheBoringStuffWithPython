c5eed0534fe10b95a0cebdfb7de33e84fff4804f
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-14 15:55:13 +0200

    continuation

--- error_example.py
+++ error_example.py
@@ -13,7 +13,9 @@ except:
     print('The traceback info was written to error_info.txt')
 
 door_status = 'open'
-assert door_status == 'open', 'Obey, door must be open!''
+assert door_status == 'open', 'Obey, door must be open!'
+door_status = 'close'
 
-# door_status = 'close'
-# assert door_status == 'open', 'Obey, door must be open!'
+
+
+assert door_status == 'open', 'Obey, door must be open!'
