f1f44226d75f69797b0835959d4e0b1ef2bf7f90
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-14 16:40:29 +0200

    continuation

--- error_example.py
+++ error_example.py
@@ -3,19 +3,16 @@ def spam():
 def bacon():
     raise Exception('This is an exception!')
 
-# 
-# import traceback
-# try:
-#     raise Exception('This is the error message!')
-# except:
-#     with open('error_info.txt', 'w') as error_file:
-#         error_file.write(traceback.format_exc())
-#     print('The traceback info was written to error_info.txt')
 
-#door_status = 'open'
-#assert door_status == 'open', 'Obey, door must be open!'
+import traceback
+try:
+    raise Exception('This is the error message!')
+except:
+    with open('error_info.txt', 'w') as error_file:
+        error_file.write(traceback.format_exc())
+    print('The traceback info was written to error_info.txt')
+
+door_status = 'open'
+assert door_status == 'open', 'Obey, door must be open!'
 door_status = 'close'
-
-
-
 assert door_status == 'open', 'Obey, door must be open!'
