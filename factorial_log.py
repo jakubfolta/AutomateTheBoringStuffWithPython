2cb1f63bd9582675c06457bf36b87531ee1ebad8
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-15 17:26:05 +0200

    continuation

--- factorial_log.py
+++ factorial_log.py
@@ -3,15 +3,15 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s\
  - %(levelname)s - %(message)s')
 #logging.disable(logging.CRITICAL)
 
-logging.debug('Start of the program.')
+logging.info('Start of the program.')
 
 def factorial(n):
     logging.debug('Start of factorial: {}'.format(n))
     total = 1
     for i in range(n + 1):
         total *= i
-        logging.debug('i is ' + str(i) + ', total is ' + str(total))
-    logging.debug('End of factorial: {}'.format(n))
+        logging.warning('i is ' + str(i) + ', total is ' + str(total))
+    logging.critical('End of factorial: {}'.format(n))
     return total
 
 print(factorial(5))
