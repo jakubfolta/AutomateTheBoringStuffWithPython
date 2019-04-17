41e8a909a74c41b2aa31ce8df282aa5fa42cd374
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-17 13:38:23 +0200

    practice project

--- debugging_coin_toss.py
+++ debugging_coin_toss.py
@@ -4,29 +4,29 @@ import logging
 logging.basicConfig(level = logging.DEBUG,\
 format = '%(asctime)s - %(levelname)s - %(message)s')
 
-guess = ''
 toss = random.randint(0, 1) # 0 is tails, 1 is heads
-toss_name = 'heads' if toss == 1 else 'tails'
-logging.info('Toss = ' + toss_name)
-while guess not in ('heads', 'tails'):
-    logging.debug('Enter the while loop.')
-    try:
-        print('Guess the coin toss! Enter "heads" or "tails":')
-        guess = input()
-        guess == 'heads' or guess == 'tails'
-        logging.info('Toss is a ' + str(toss))
-    except:
-        continue
+toss = 'heads' if toss == 1 else 'tails'
 
+logging.info('Toss = ' + toss)
 
+def check_guess():
+    guess = ''
+    while guess not in ('heads', 'tails'):
+        logging.debug('Enter the while loop.')
+        try:
+            print('Guess the coin toss! Enter "heads" or "tails":')
+            guess = input()
+            guess == 'heads' or guess == 'tails'
+            logging.info('Toss is a ' + str(toss))
+        except:
+            continue
+    return guess
 
-logging.debug('Out of loop.')
-if toss == guess:
+if toss == check_guess():
     print('You got it!')
 else:
     print('Nope! Guess again!')
-    guesss = input()
-    if toss == guess:
+    if toss == check_guess():
        print('You got it!')
     else:
         print('Nope. You are really bad at this game.')
