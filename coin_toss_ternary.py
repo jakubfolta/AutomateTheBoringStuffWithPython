dc59e967ba1bca79864b2a76a1be98f24e6ee3ee
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-19 21:14:25 +0200

    practice project-solidifying

--- coin_toss_ternary.py
+++ coin_toss_ternary.py
@@ -15,14 +15,14 @@ toss = random.randint(0,1)
 toss = 'tails' if toss == 0 else 'heads'
 
 while toss not in sides:
+    logging
     print('Guess the coin toss! Enter heads or tails:')
     guess = input()
-    if toss == guess:
-    print('You got it!')
-else:
-    print('Nope! Guess again!')
-    guesss = input()
+    print('You got it!') if toss == guess else print('Nope! Guess again!')
+    if toss == guess: print()
+    else:
+        guesss = input()
+        print('You got it!') if toss == guess else print('Nope. You are really bad at this game.')
     if toss == guess:
        print('You got it!')
     else:
-        print('Nope. You are really bad at this game.')
