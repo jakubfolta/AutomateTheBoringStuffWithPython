42d072f260e3896f64be92ad52e5722a19b78c77
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-20 16:01:34 +0200

    removing bugs project-refactoring

--- coin_toss.py
+++ coin_toss.py
@@ -11,21 +11,24 @@ logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s, %(levelname)s,
 #logging.disable(logging.INFO)
 
 guess = ''
+chance = 0
 toss = random.randint(0, 1) # 0 is tails, 1 is heads
 toss = 'tails' if toss==0 else 'heads'
-logging.info('Toss is ' + str(toss))
+logging.info('Toss is a {}.'.format(toss))
 sides = ['heads', 'tails']
 
-while guess not in sides:
+while guess not in sides or chance != 2:
     print('Guess the coin toss! Enter heads or tails:')
     guess = input()
 
     if guess not in sides: continue
 
-    if toss == guess: print('You got it!'), logging.info('Guessed, out of loop.')
+    print('You got it!') if toss == guess else print('Nope!')
+
+    logging.info('Guessed, out of loop.')
 
     else:
         logging.info('Second guess in else.')
-        print('Nope! Guess again!')
+
         guess = input()
         print('You got it!') if toss == guess else print('Nope. You are really bad at this game.')
