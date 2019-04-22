35eeb481683e8ebab063593afe5621d2eb673fc6
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-22 17:04:08 +0200

    solidifying knowledge

---
+++ coin_toss_solidifying.py
@@ -0,0 +1,11 @@
+#! python3
+
+
+'''debugging_coin_toss.py - Simple coin toss game, the player has\
+two guesses(simple game). The program has several bugs.\
+Find bugs that keep the program from working correctly.'''
+
+# TODO: Import essential modules.
+# TODO: Set the sides of coin.
+# TODO: Set the coin toss and sidename - ternary operator.
+# TODO: Use while loop to give two guesses and check if input is a proper name.
