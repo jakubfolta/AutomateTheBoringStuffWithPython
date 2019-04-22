b2cafe44624e5d013f1b8dba39541920d8664d1c
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-22 17:07:59 +0200

    solidifying knowledge

--- coin_toss_solidifying.py
+++ coin_toss_solidifying.py
@@ -6,6 +6,12 @@ two guesses(simple game). The program has several bugs.\
 Find bugs that keep the program from working correctly.'''
 
 # TODO: Import essential modules.
+import random
+import logging
+
+logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s, %(levelname)s, %(message)s')
+#logging.disable(logging.WARNING)
+
 # TODO: Set the sides of coin.
 # TODO: Set the coin toss and sidename - ternary operator.
 # TODO: Use while loop to give two guesses and check if input is a proper name.
