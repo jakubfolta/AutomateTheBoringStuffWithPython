743e54634dd8b50bc692850399e33471c3d7897d
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-23 17:10:08 +0200

    solidifying knowledge

--- coin_solidifying.py
+++ coin_solidifying.py
@@ -10,6 +10,12 @@ import logging
 logging.basicConfig(level = logging.INFO, format = '%(levelname)s, %(message)s')
 #logging.disable(logging.CRITICAL)
 
-# TODO: Set essential variables: guess, number, toss, sides.
-# TODO: Set toss to one of two coin sides.
+# Set essential variables: guess, number, toss, sides.
+guess = ''
+number = 0
+toss = random.randint(0, 1) # 0 is heads, 1 is tails.
+
+# Set toss to one of two coin sides.
+toss = 'heads' if toss == 0 else 'tails'
+
 # TODO: Use while loop to check if user guess is right and act appropriately.
