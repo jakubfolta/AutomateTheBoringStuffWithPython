ec2b4663fd8e005e2c302e131d31a599c2f9a433
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-23 17:02:15 +0200

    solidifying knowledge

--- coin_solidifying.py
+++ coin_solidifying.py
@@ -5,9 +5,11 @@ two guesses(simple game). The program has several bugs.\
 Find bugs that keep the program from working correctly.'''
 
 # TODO: Import essential modules.
+import random
+import logging
+logging.basicConfig(level = logging.INFO, format = '%(levelname)s, %(message)s')
+#logging.disable(logging.CRITICAL)
+
 # TODO: Set essential variables: guess, number, toss, sides.
 # TODO: Set toss to one of two coin sides.
-# TODO:
-# TODO:
-# TODO:
-# TODO:
+# TODO: Use while loop to check if user guess is right and act appropriately.
