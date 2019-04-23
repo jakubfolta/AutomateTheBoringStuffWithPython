e4e1ec738d36610169004246b6d63058c31e3690
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-23 16:51:27 +0200

    solidifying knowledge

---
+++ coin_solidifying.py
@@ -0,0 +1,13 @@
+#! python3
+
+'''coin_solidifying.py - Simple coin toss game, the player has\
+two guesses(simple game). The program has several bugs.\
+Find bugs that keep the program from working correctly.'''
+
+# TODO: 
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
