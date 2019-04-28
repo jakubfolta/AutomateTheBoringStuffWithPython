8fa3049a61303ca0da0bd25ad0095b0c35ae816b
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-28 18:51:17 +0100

    continue

--- web_scraping.py
+++ web_scraping.py
@@ -1,6 +1,7 @@
 import webbrowser
 #webbrowser.open('http://inventwithpython.com/')
 import requests
+import bs4
 
 # res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
 # print(type(res))
@@ -8,8 +9,21 @@ import requests
 # print(len(res.text))
 # print(res.text[:250])
 
-res = requests.get('http://inventwithpython.com/page_that_does_not_exists')
-try:
-    res.raise_for_status()
-except Exception as exc:
-    print('There was a problem: {}'.format(exc))
+# res = requests.get('http://inventwithpython.com/page_that_does_not_exists')
+# try:
+#     res.raise_for_status()
+# except Exception as exc:
+#     print('There was a problem: {}'.format(exc))
+
+# res = requests.get('http://automatetheboringstuff.com/files/rj.txt')
+# res.raise_for_status()
+# play_file = open('RomeoAndJuliet.txt', 'wb')
+# for chunk in res.iter_content(100000):
+#     play_file.write(chunk)
+#
+# play_file.close()
+
+res = requests.get('http://nostarch.com')
+res.raise_for_status()
+no_starch_soup = bs4.BeautifulSoup(res.text)
+print(type(no_starch_soup))
