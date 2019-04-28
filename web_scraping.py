a707914beea21483fa297aba3f5ed3989f558123
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-28 14:22:09 +0100

    continue

--- web_scraping.py
+++ web_scraping.py
@@ -1,3 +1,9 @@
 import webbrowser
 #webbrowser.open('http://inventwithpython.com/')
 import requests
+
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+print(type(res))
+res.status_code == requests.codes.ok
+print(len(res.text))
+print(res.text[:250])
