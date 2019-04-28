44dc3d7181b83ede34d6c3c9f5aef7d989f30207
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-28 14:30:15 +0100

    continue

--- web_scraping.py
+++ web_scraping.py
@@ -2,8 +2,11 @@ import webbrowser
 #webbrowser.open('http://inventwithpython.com/')
 import requests
 
-res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
-print(type(res))
-res.status_code == requests.codes.ok
-print(len(res.text))
-print(res.text[:250])
+# res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+# print(type(res))
+# res.status_code == requests.codes.ok
+# print(len(res.text))
+# print(res.text[:250])
+
+res = requests.get('http://inventwithpython.com/page_that_does_not_exists')
+res.raise_for_status()
