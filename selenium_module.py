e84641be851f2cbc71f3bf9caee3635029c7b2cf
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-05 21:12:18 +0100

    continue

--- selenium_module.py
+++ selenium_module.py
@@ -6,3 +6,10 @@
 #     print('Found {} element with that class name!'.format(elem.tag_name))
 # except:
 #     print('Was not able to find an element with that name.')
+
+from selenium import webdriver
+browser = webdriver.Firefox()
+browser.get('http://inventwithpython.com')
+link_element = browser.find_elements_by_link_text('More Info')
+print(type(link_element))
+link_element[5].click()
