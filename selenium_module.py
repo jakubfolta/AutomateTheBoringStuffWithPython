39d2cbb22f330022fcc65265c7433e1e2f3ac76e
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-05 20:40:11 +0100

    continue

--- selenium_module.py
+++ selenium_module.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
 browser = webdriver.Firefox()
-print(type(browser))
 browser.get('http://inventwithpython.com')
 try:
-    elem = browser.find_element_by_class_name('bookcover')
+    elem = browser.find_element_by_class_name('card-img-top')
     print('Found {} element with that class name!'.format(elem.tag_name))
 except:
     print('Was not able to find an element with that name.')
